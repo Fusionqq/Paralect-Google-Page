5eb4444e86ff882d8901ceace5c6d4cbb2adaf74
Osipov Alexander <fusion96@mail.ru>
2017-05-11 17:08:15 +0300

    final

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
-
-    /*var docWidth = $(window).width();
-	var leftWidth = $('.leftBlock').width();
-	var rightWidth = $('.rightBlock').width();
-	var len = docWidth - leftWidth - rightWidth - 30;
-	$('.rightBlock').css({'margin-left' : len});*/
 	var docHeight = $(window).height();
 	$('.logo').css({'margin-top' : ((docHeight / 2) - 270)});
 	if (((docHeight / 2) - 270) <= 0) {
@@ -14,11 +8,6 @@ $(document).ready(function() {
 });
 
 $(window).resize(function () {
-	/*var docWidth = $(window).width();
-	var leftWidth = $('.leftBlock').width();
-	var rightWidth = $('.rightBlock').width();
-	var len = docWidth - leftWidth - rightWidth - 30;
-	$('.rightBlock').css({'margin-left' : len});*/
 	var docHeight = $(window).height();
 	$('.logo').css({'margin-top' : ((docHeight / 2) - 270)});
 	if (((docHeight / 2) - 270) <= 0) {
